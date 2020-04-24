f84e5818cff87c60cdba47572c805c273fefe482
Diego <dflores7237@gmail.com>
2020-04-24 16:37:36 -0700

    functional project display component

--- src/ProjectComp.jsx
+++ src/ProjectComp.jsx
@@ -12,8 +12,11 @@ function ProjectComp(props) {
             <div className='projectTitle'>Project Title</div>
             <div className='projectBuildTools'> Comp1 and Comp2</div>
             <div className='projectDescription'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</div>
-            <div className='projectLinks'>Github</div>
-            <div className='projectLinks'>View Live</div>
+            <div className='projectLinksContainer'>
+                <a className='projectLinks' href='https://www.github.com' target='_blank'>Github</a>
+                <br/>
+                <a className='projectLinks' href='https://www.google.com' target='_blank'>View Live</a>
+            </div>
         </div>
     </div>
   )
